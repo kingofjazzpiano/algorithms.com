fb28943d13e16a8618b106a00483355600ea9fec
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-10-25 05:42:10 +0000

    show localhost/docs

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', RedirectView.as_view(url='/static/docs/index.html', permanent=False)),
 ]
