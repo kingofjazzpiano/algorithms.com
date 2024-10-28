e080c5b49f721384d511715dbb15b0a43e48b5fa
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-10-28 10:17:27 +0000

    try to run structurizr

--- documentation/urls.py
+++ documentation/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
             url="../static/documentation/html/index.html",
             permanent=False)),
     path('architecture/', views.architecture_view, name='architecture'),
+    path('architecture/structurizr/', views.struct_proxy,
+         name="structurizr_proxy")
 ]
--- documentation/views.py
+++ documentation/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+import requests
 
 
 def architecture_view(request):
     return render(request, 'documentation/architecture.html')
+
+
+@csrf_exempt
+def struct_proxy(request):
+
+    # URL of the Structurizr instance
+    struct_url = "http://localhost:8080"
+
+    # Forward the request to Structurizr
+    response = requests.get(struct_url, headers=request.headers,
+                            cookies=request.COOKIES)
+
+    return HttpResponse(response.content, status=response.status_code,
+                        content_type=response.headers["Content-Type"])
