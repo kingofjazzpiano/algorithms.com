767d50e06a3c91de8ec73c5630630d77b2bc783a
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-10-29 17:46:48 +0000

    simplification architecture page

--- documentation/urls.py
+++ documentation/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
             url="../static/documentation/html/index.html",
             permanent=False)),
     path('architecture/', views.architecture_view, name='architecture'),
-    path('architecture/structurizr/', views.struct_proxy,
-         name="structurizr_proxy")
 ]
---
+++ documentation/view.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def architecture_view(request):
+    return render(request, 'architecture.html')
