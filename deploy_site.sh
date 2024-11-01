2d87785bbc211ea8498cb8dbc31324c663003e23
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-11-01 16:54:15 +0000

    add deploy script

---
+++ deploy_site.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias rm='rm -I'
+source ~/.bashrc
+sudo apt update
+sudo apt install micro -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo | sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce -y
