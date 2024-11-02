65f0cc05ab80b3d5c4b5e94bdbb03f9b3e81ad50
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-11-02 11:27:07 +0100

    some updates+

--- deploy_site.sh
+++ deploy_site.sh
@@ -9,3 +9,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo | sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce -y
+docker build -t algorithms.com .
