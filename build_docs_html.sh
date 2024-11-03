a00664d36763f7f0d8c45759e80bcbc692e12a62
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-11-03 17:29:08 +0100

    add clear static/documentation before copying

--- build_docs_html.sh
+++ build_docs_html.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+STATIC_DIR="static/documentation"
+
 # Navigate to the Sphinx documentation directory
 cd documentation/sphinx_docs/ || { echo "Documentation directory not found"; exit 1; }
 
@@ -9,10 +11,14 @@ make html
 # Go back to the project root
 cd ../../
 
-# Create the static/documentation directory if it doesn't exist
-if [ ! -d static/documentation/ ]; then
-    mkdir -p static/documentation/
+if [ -d "$STATIC_DIR" ]; then
+    echo "Clearing existing documentation in $STATIC_DIR..."
+    rm -rf $STATIC_DIR
+else
+    echo "Creating $STATIC_DIR directory..."
+    mkdir -p $STATIC_DIR
 fi
 
 # Copy the generated HTML files to the static directory
-cp -r documentation/sphinx_docs/build/html static/documentation/
+echo "Copying HTML documentation to $STATIC_DIR..."
+cp -r documentation/sphinx_docs/build/html $STATIC_DIR
