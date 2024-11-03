e43b74b2da99a153cce2b003bcdd2035f5ad626d
kingofjazzpiano <mrkingofjazzpiano@gmail.com>
2024-11-03 16:12:08 +0100

    working script for build docs

--- build_docs_html.sh
+++ build_docs_html.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-documentation/sphinx_docs/make html
-cp -r documentation/sphinx_docs/build/html/* static/documentation/
+# Navigate to the Sphinx documentation directory
+cd documentation/sphinx_docs/ || { echo "Documentation directory not found"; exit 1; }
+
+# Run make to build the HTML documentation
+make html
+
+# Go back to the project root
+cd ../../
+
+# Create the static/documentation directory if it doesn't exist
+if [ ! -d static/documentation/ ]; then
+    mkdir -p static/documentation/
+fi
+
+# Copy the generated HTML files to the static directory
+cp -r documentation/sphinx_docs/build/html static/documentation/
--- deploy_site.sh
+++ deploy_site.sh
@@ -9,7 +9,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo | sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce -y
-docker build --no-cache -t algorithms.com . --progress=plain | tee build_image.log
 docker rm -f algocontainer
 docker rmi -f algorithms.com
-docker run -it -p 8000:8000 --name algocontainer algorithms.com
+docker build --no-cache -t algorithms.com . --progress=plain | tee build_image.log
+docker run -it -p 8000:8000 --name algocontainer algorithms.com /bin/bash
